617ed6a9faa644988a83deded7fd00a7fa5a8cf5
Connor Marble <Marbleconnor@gmail.com>
2014-10-17 10:00:54 -0400

    added scroll buttons to reader screen and removed old scroll behavior

--- Android_Tlog_Utils/screens/ReadLog.py
+++ Android_Tlog_Utils/screens/ReadLog.py
@@ -1,21 +1,23 @@
 from kivy.uix.widget import Widget
 from kivy.properties import Property
-
+import math
 
 class Reader(Widget):
     log = None
     log_text = Property('<Log_Text>')
-    packetlimit = 150
+    lines = 25 #TODO get number of lines based on screen height
+    scroll = 0
+
+    def _init__(self,**kwargs):
+        super(Reader,self).__init__(kwargs)
 
     def DisplayLog(self):
         log_text = ''
-        for i in range(len(self.log)):
-            if i < self.packetlimit:
-                log_text += str(self.log[i])
+        for i in range(self.lines):
+            if i < self.lines:
+                log_text += str(self.log[i+scroll])
                 log_text += '\n'
 
-            if i >= self.packetlimit:
-                log_text += "\n \n \n  ... Output truncated to " +\
-                str(self.packetlimit) + " packets"
-                break
         self.log_text.text = log_text
+
+    
