b866e0148312b9c6123919f3a1b039f477fe0c9c
Connor Marble <Marbleconnor@gmail.com>
2014-10-24 20:10:00 -0400

    Adjust position and size of text label depending on the screen and button sizes

--- Android_Tlog_Utils/screens/ReadLog.py
+++ Android_Tlog_Utils/screens/ReadLog.py
@@ -13,15 +13,19 @@ class Reader(Widget):
 
     scroll_down = ObjectProperty(Button)
     scroll_up = ObjectProperty(Button)
-
-    lines = 25 #TODO get number of lines based on screen height
+        
     scrollpos = 0
 
     def __init__(self,**kwargs):
         super(Reader,self).__init__()
         self.scroll_down.bind(on_press=self.scroll)
         self.scroll_up.bind(on_press=self.scroll)
-
+        self.lines=super(Reader,self).height/7
+        #TODO account for multi-line packages
+        self.log_text.y = self.scroll_down.height
+        
+        
+        
     def DisplayLog(self):
         log_text = ''
         for i in range(self.lines):
@@ -30,6 +34,7 @@ class Reader(Widget):
                 log_text += '\n'
 
         self.log_text.text = log_text
+        
     
     def scroll(instance, value):
         buttonText = value.text
@@ -37,5 +42,3 @@ class Reader(Widget):
         instance.scrollpos += 1 if buttonText == 'V' else -1
         
         instance.DisplayLog()
-
-    
