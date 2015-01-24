07aadc85b151c5516dc2fd09c4a62abe4183696e
Connor Marble <Marbleconnor@gmail.com>
2015-01-24 18:35:43 -0500

    added functionality to save basic kml file

--- TelemetryViewer/kml_gen.py
+++ TelemetryViewer/kml_gen.py
@@ -1,7 +1,10 @@
 import mav_parse as mp
+from pykml.factory import KML_ElementMaker as KML
 from libs.Mavlink.apm_mavlink_v1 import MAVLink_global_position_int_message as position 
+from lxml import etree
 
 def tlog_to_kml(log, output):
+    
     isrelaventpacket = lambda x:type(x) is position
     
     locations = []
@@ -9,13 +12,19 @@ def tlog_to_kml(log, output):
 
     locations = [(x.lat, x.lon) for x in locationpackets]
 
-    with open(output, 'w') as kml_file:
-        pass #TODO: draw path of netquad in file using locations list
+    kmldoc = KML.kml(
+        KML.Placemark(
+            KML.name("kml_name")))
+
     
-    print locations
+    with open(output, 'w') as kml_file:
+        kml_file.write(etree.tostring(kmldoc, pretty_print=True))
+    
+
             
 
 if __name__=='__main__':
     testpath = '/Logs/2014-09-29 14-14-38.tlog'
     log = mp.TelemetryLog(testpath).ParsePackets()
     tlog_to_kml(log, 'test.kml')
+    
